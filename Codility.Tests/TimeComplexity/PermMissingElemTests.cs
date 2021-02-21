6a8f87faa384326102812f8269f93142c6aef55a
Fabricio Oliveira Silva <Fabricio_Oliveira_si@Dell.com>
2021-02-21 15:02:16 -0300

    PermMissingElem

---
+++ Codility.Tests/TimeComplexity/PermMissingElemTests.cs
@@ -0,0 +1,93 @@
+﻿using Codility.TimeComplexity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Codility.Tests.TimeComplexity
+{
+    [TestClass]
+    public class PermMissingElemTests
+    {
+        [TestMethod]
+        public void LongArrayTest()
+        {
+            int maxElement = 100000;
+            int[] entryArray = new int[maxElement - 1];
+            for (int i = 1; i < maxElement ; i++)
+            {
+                entryArray[i-1] = i + 1;
+            }
+          
+            var permMissingSolver = new PermMissingElem();
+            int solution = permMissingSolver.solution(entryArray);
+            Assert.AreEqual(solution,1);
+        }
+
+        [TestMethod]
+        public void LongMissingRandomElement()
+        {
+            var permMissingSolver = new PermMissingElem();
+
+            int maxElement = 100000;
+            List<int> entryArray = new List<int>();
+            for (int i = 0; i < maxElement; i++)
+            {
+                entryArray.Add(i + 1);
+            }
+
+            var rnd = new Random();
+            int randomMissingElement = rnd.Next(1, maxElement);
+            entryArray.RemoveAt(randomMissingElement - 1);
+
+            int solution = permMissingSolver.solution(entryArray.ToArray());
+            Assert.AreEqual(solution, randomMissingElement);
+        }
+
+        [TestMethod]
+        public void ShortArrayMissingFirstElement()
+        {
+            var permMissingSolver = new PermMissingElem();
+
+            int maxElement = 5;
+            int[] entryArray = new int[maxElement - 1];
+            for (int i = 1; i < maxElement; i++)
+            {
+                entryArray[i - 1] = i + 1;
+            }
+
+            int solution = permMissingSolver.solution(entryArray);
+            Assert.IsTrue(solution == 1);
+
+
+            maxElement = 1000;
+            entryArray = new int[maxElement - 1];
+            for (int i = 1; i < maxElement; i++)
+            {
+                entryArray[i - 1] = i + 1;
+            }
+
+            solution = permMissingSolver.solution(entryArray);
+            Assert.IsTrue(solution == 1);
+        }
+
+        [TestMethod]
+        public void ShortArrayMissingRandomElement()
+        {
+            var permMissingSolver = new PermMissingElem();
+
+            int maxElement = 100;
+            List<int> entryArray = new List<int>();
+            for (int i = 0; i < maxElement; i++)
+            {
+                entryArray.Add(i + 1);
+            }
+
+            var rnd = new Random();
+            int randomMissingElement = rnd.Next(1, maxElement);
+            entryArray.RemoveAt(randomMissingElement - 1 );
+
+            int solution = permMissingSolver.solution(entryArray.ToArray());
+            Assert.AreEqual(solution,randomMissingElement);
+        }
+    }
+}
---
+++ Codility/TimeComplexity/PermMissingElem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.TimeComplexity
+{
+    public class PermMissingElem
+    {
+        //https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
+        public int solution(int[] A)
+        {
+            if(A.Length > 10000)
+            {
+                long totalSum = 0;
+                long expectedSum;
+                if ((A.Length + 1) % 2 == 0)
+                {
+                    expectedSum = Math.BigMul(((A.Length + 1) / 2), A.Length + 2);
+                }
+                else
+                {
+                    expectedSum = Math.BigMul(((A.Length + 2) / 2), A.Length + 1);
+                }
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    totalSum += A[i];
+                }
+
+                return Convert.ToInt32(expectedSum - totalSum);
+            }
+            else
+            {
+                int totalSum = 0;
+                int expectedSum = (A.Length + 1) * (A.Length + 2) / 2;
+                for (int i = 0; i < A.Length; i++)
+                {
+                    totalSum += A[i];
+                }
+
+                return expectedSum - totalSum;
+            }
+        }
+    }
+}
