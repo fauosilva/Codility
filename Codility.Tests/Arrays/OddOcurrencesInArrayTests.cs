7f314ccfc539330072f3d5841472789e2563cef9
Fabricio Oliveira Silva <Fabricio_Oliveira_si@Dell.com>
2021-02-21 12:03:56 -0300

    Adding simple tests and implementation of OddOcurrencesInArray

---
+++ Codility.Tests/Arrays/OddOcurrencesInArrayTests.cs
@@ -0,0 +1,25 @@
+﻿using Codility.Iterations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tests.Arrays
+{
+    [TestClass]
+    public class OddOcurrencesInArrayTests
+    {
+        [TestMethod]
+        public void TestOddOcurrences()
+        {
+            int[] sampleArray = new int[] { 9, 3, 9, 3, 9, 7, 9 };
+            var OddOccurrencesSolver = new OddOccurrencesInArray();
+
+            var solution =  OddOccurrencesSolver.Solution(sampleArray);
+
+            Assert.AreEqual(solution, 7);
+        }
+    }
+}
---
+++ Codility/Iterations/OddOccurrencesInArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Iterations
+{
+    //https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+    public class OddOccurrencesInArray
+    {
+        public int Solution(int[] A)
+        {
+            var resultDictionary = new Dictionary<int, IList<int>>();
+            for(int i =0; i < A.Length; i++)
+            {
+                AddToDictionary(resultDictionary, A[i], i);
+            }
+            return resultDictionary.Keys.First();
+        }
+
+        private void AddToDictionary(Dictionary<int,IList<int>> resultDictionary, int value, int index)
+        {
+            if (!resultDictionary.ContainsKey(value))
+            {
+                resultDictionary[value] = new List<int>() { index };
+            }
+            else
+            {
+                //Found a duplicate of the odd number.
+                resultDictionary.Remove(value);
+            }
+        }
+    }
+}
