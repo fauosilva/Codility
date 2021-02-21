0ffc9ae75e9a19a6d47c38a6278f2c6145f749bf
Fabricio Oliveira Silva <Fabricio_Oliveira_si@Dell.com>
2021-02-21 19:42:47 -0300

    [WIP] TapeEquilibrium

---
+++ Codility.Tests/TimeComplexity/TapeEquilibriumTests.cs
@@ -0,0 +1,61 @@
+﻿using Codility.TimeComplexity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tests.TimeComplexity
+{
+    [TestClass]
+    public class TapeEquilibriumTests
+    {
+        [TestMethod]
+        public void TestShortTape()
+        {
+            var tapeEquilibriumSolver = new TapeEquilibrium();
+
+            int maxElement = 10;
+            List<int> entryArray = new List<int>();
+            for (int i = 0; i < maxElement; i++)
+            {
+                entryArray.Add(i + 1);
+            }
+
+            int solution = tapeEquilibriumSolver.solution(entryArray.ToArray());
+            Assert.AreEqual(solution, 1);
+        }
+
+
+        [TestMethod]
+        public void TestShortTapeSample()
+        {
+            var tapeEquilibriumSolver = new TapeEquilibrium();
+            List<int> entryArray = new List<int>() { 3, 1, 2, 4, 3 };
+        
+            int solution = tapeEquilibriumSolver.solution(entryArray.ToArray());
+            Assert.AreEqual(solution, 1);
+        }
+
+        [TestMethod]
+        public void TestShortTapeSkipingOneElement()
+        {
+            var tapeEquilibriumSolver = new TapeEquilibrium();
+
+            int maxElement = 10;
+            List<int> entryArray = new List<int>();
+            for (int i = 0; i < maxElement; i++)
+            {
+                entryArray.Add(i + 1);
+            }
+
+            var rnd = new Random();
+            int randomMissingElement = rnd.Next(1, maxElement);
+            entryArray.RemoveAt(randomMissingElement - 1);
+
+            int solution = tapeEquilibriumSolver.solution(entryArray.ToArray());
+            Assert.AreEqual(solution, 1);
+        }
+    }
+}
---
+++ Codility/TimeComplexity/TapeEquilibrium.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.TimeComplexity
+{
+    public class TapeEquilibrium
+    {
+        //[10,11]
+        public int solution(int[] A)
+        {
+            long[] totalSumFwd = new long[A.Length];
+            long[] totalSumBwk = new long[A.Length];
+            
+            totalSumFwd[0] = A[0];
+            totalSumBwk[0] = A[A.Length - 1];
+
+            long minDif = int.MaxValue;
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                totalSumFwd[i] = totalSumFwd[i - 1] + A[i];
+                totalSumBwk[i] = totalSumBwk[i - 1] + A[A.Length - i - 1];
+            }
+
+            for (int j = 1; j < A.Length; j++)
+            {
+                var walkOneForward = Math.Abs(totalSumFwd[j] - totalSumBwk[A.Length - j]);
+                if (walkOneForward < minDif)
+                    minDif = walkOneForward;
+            }
+
+            return Convert.ToInt32(minDif);
+        }
+    }
+}
