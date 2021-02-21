c750e4cf74fdd03f1aeccff0938232277f494c4c
Fabricio Oliveira Silva <Fabricio_Oliveira_si@Dell.com>
2021-02-20 21:01:23 -0300

    Cyclic rotation tests and implementation

---
+++ Codility.Tests/Arrays/CyclicRotationsTests.cs
@@ -0,0 +1,29 @@
+﻿using Codility.Arrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.Tests.Arrays
+{
+    [TestClass]
+    public class CyclicRotationsTests
+    {
+        [TestMethod]
+        public void SimpleRotations()
+        {
+            //Arrange
+            var CyclicRotationSolver = new CyclicRotation();
+            //Act
+            var TestArray = new int[] { 3, 8, 9, 7, 6 };
+            var RotatedArray = new int[] { 9, 7, 6, 3, 8 };
+            var rotation = 3;
+
+            //Test
+            var rotationResult = CyclicRotationSolver.Solution(TestArray, rotation);
+
+            Assert.AreEqual(rotationResult.Length, RotatedArray.Length);
+            for(int i = 0; i< rotationResult.Length; i++)
+            {
+                Assert.AreEqual(rotationResult[i], RotatedArray[i]);
+            }
+        }
+    }
+}
---
+++ Codility/Arrays/CyclicRotation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Arrays
+{
+    public class CyclicRotation
+    {
+        public int[] Solution (int[] A, int K)
+        {
+            Console.WriteLine($"Entry Array: {A}");
+
+            if (K == 0 || A == null || A.Length == 0)
+                return A;
+
+            int mod = K % A.Length;
+            if (mod == 0)
+                return A;
+
+            int[] returnArray = new int[A.Length];
+            for(int i = 0; i < A.Length; i++)
+            {
+                int modPosition = (i + mod) % A.Length;
+                if (modPosition - 1 >= 0)
+                    returnArray[i] = A[modPosition - 1];
+                else
+                    returnArray[i] = A[A.Length -1];
+            }
+
+            Console.WriteLine($"Return Array: {A}");
+            return returnArray;
+        }
+    }
+}
+
