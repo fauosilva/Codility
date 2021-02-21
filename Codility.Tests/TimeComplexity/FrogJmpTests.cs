6a05d67777bc5e5b2e080e9ee732828ae5827c61
Fabricio Oliveira Silva <Fabricio_Oliveira_si@Dell.com>
2021-02-21 12:50:32 -0300

    Adding frogjmp algorithm

---
+++ Codility.Tests/TimeComplexity/FrogJmpTests.cs
@@ -0,0 +1,19 @@
+﻿using Codility.TimeComplexity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.Tests.TimeComplexity.cs
+{
+    [TestClass]
+    public class FrogJmpTests
+    {
+        [TestMethod]
+        public void TestFrogJmp()
+        {
+            var frogJmpSolver = new FrogJmp();
+
+            var solution = frogJmpSolver.solution(1, 10, 2);
+
+            Assert.AreEqual(solution, 5);
+        }
+    }
+}
---
+++ Codility/TimeComplexity/FrogJmp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Codility.TimeComplexity
+{
+    public class FrogJmp
+    {
+        public int solution(int X, int Y, int D)
+        {
+            if (X == Y)
+                return 0;
+
+            decimal distanceToCover = Y - X;
+            decimal minimumJumps = Math.Ceiling(distanceToCover / D);
+            return Convert.ToInt32(minimumJumps);
+        }
+    }
+}
